626146e94a149255162325dd6e8eeb6f6fd71ccf
KarenRouge <karenrouge1991@gmail.com>
2023-01-28 17:50:37 +0800

    Task new

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string[] M;
+﻿string[] Massive;
             Console.WriteLine("Введите массив строк через пробел");
             string stroka = Console.ReadLine();
-
+ Massive = stroka.Split(' ');
+            var result = new string[M .Length];
+            var realSize = 0;
