b60d2ac66e2abacd3e251360f296358c2f2d5ea6
KarenRouge <karenrouge1991@gmail.com>
2023-01-28 17:56:50 +0800

    task complete

--- Program.cs
+++ Program.cs
@@ -2,5 +2,15 @@
             Console.WriteLine("Введите массив строк через пробел");
             string stroka = Console.ReadLine();
  Massive = stroka.Split(' ');
-            var result = new string[M .Length];
-            var realSize = 0;
+            var result = new string[Massive.Length];
+            var Size = 0;
+ foreach (var line in Massive)
+            {
+                if (line.Length <= 3)
+                {
+                    result[Size] = line;
+                    Size++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, Size));
+            Console.ReadKey(true);
